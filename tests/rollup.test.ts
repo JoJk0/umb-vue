67a2bd2706ebf74e93fc345cfce96eba0891a6a0
Jacob Janisz <jacob.janisz@weightmans.com>
2025-02-11 13:48:34 +0000

    test: fix

--- tests/rollup.test.ts
+++ tests/rollup.test.ts
@@ -1,13 +1,13 @@
 import path from 'node:path'
-import { rollupBuild, testFixtures } from '@sxzz/test-utils'
+import { testFixtures } from '@sxzz/test-utils'
 import { describe } from 'vitest'
-import UnpluginStarter from '../src/rollup'
+// import UnpluginStarter from '../src/rollup'
 
 describe('rollup', async () => {
   const { dirname } = import.meta
   await testFixtures(
     '*.js',
-    async (args, id) => {
+    () => {
       // const { snapshot } = await rollupBuild(id, [UnpluginStarter()])
       // return snapshot
       return {}
