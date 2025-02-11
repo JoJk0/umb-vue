194842e09685aa094cd495a2d1c761881d368a12
Jacob Janisz <jacob.janisz@weightmans.com>
2025-02-11 13:53:49 +0000

    test: fix

--- tests/rollup.test.ts
+++ tests/rollup.test.ts
@@ -1,9 +1,12 @@
 // import path from 'node:path'
 // import { testFixtures } from '@sxzz/test-utils'
-import { describe } from 'vitest'
+import { describe, expect, it } from 'vitest'
 // import UnpluginStarter from '../src/rollup'
 
-describe('rollup', async () => {
+describe('rollup', () => {
+  it('should work', () => {
+    expect(true).toBe(true)
+  })
   // const { dirname } = import.meta
   // await testFixtures(
   //   '*.js',
